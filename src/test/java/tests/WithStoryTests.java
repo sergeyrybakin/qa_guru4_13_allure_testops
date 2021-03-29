546a6a996a7cb8c3e4a90cced52736bbaa218325
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-03-29 20:49:05 +0700

    add WithStiryTest

---
+++ src/test/java/tests/WithStoryTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("web")
+@Feature("Login tests")
+@Story("Some story")
+public class WithStoryTests extends TestBase {
+    @Test
+    @DisplayName("Some fake test with story")
+    void storyTest() {
+        assertTrue(true);
+    }
+}
